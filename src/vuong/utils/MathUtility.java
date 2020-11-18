cdbbbb73f3e6a0b0e43a2cfea99d59468af88277
VoThanhVuong <vuonglocan2602@gmail.com>
2020-11-18 08:00:14 +0700

    optimize the getF() by using recursion

--- src/vuong/utils/MathUtility.java
+++ src/vuong/utils/MathUtility.java
@@ -13,17 +13,22 @@ public class MathUtility {
 
     public static final double PI = 3.1415;
 
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException("u must be between 0...20");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long result = 1;
+//        for(int i=2; i<=n;i++){
+//            result*=i;
+//        }
+//        return result;
+//    }  
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20) {
-            throw new IllegalArgumentException("u must be between 0...20");
-        }
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        long result = 1;
-        for(int i=2; i<=n;i++){
-            result*=i;
-        }
-        return result;
-    }  
+        if(n<0||n>20) throw new IllegalArgumentException("n must be between 0...20");
+        if(n==0||n==1) return 1;
+        return n*getFactorial(n-1);
+    }
 }
